f8756a4e0f00eac5d0cbc0e7a275629a9993857a
Speedweener <www.kwekzhanhao97995074@gmail.com>
2021-09-09 16:44:33 +0800

    Before AJAX integration

--- Web/app.py
+++ Web/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, url_for, jsonify
+from flask import Flask, render_template, url_for, jsonify, request
 import numpy as np
 import gravity_plot
 
 app = Flask(__name__)
 lat_sample, long_sample, values = gravity_plot.generate_values()
-polygons, c_bar, lat, long = gravity_plot.gmap_save(lat_sample, long_sample, values)
+polygons, c_bar, lat, long = gravity_plot.gmap_output(lat_sample, long_sample, values)
 
 # @app.route('/update_decimal', methods=['POST'])
 # def update_decimal():
